1b148438f39183444e0ad1d2039dc070534aaaa3
justttry <kerzhao@163.com>
2017-06-30 11:41:15 +0800

    update

--- ctc_captcha_break.py
+++ ctc_captcha_break.py
@@ -131,15 +131,21 @@ job = Job("3Q20LA3MXU3N8Y9NVJ7A1T5WNHL2IWQSNNJ5V9I5P7MRJ8LSC33EN2DT3EWYLCJA",
           1000,
           10)
 
+import time
 print '------------------------test0----------------------------'
+print time.ctime()
 
 # 启动任务
 job.send_with_files('http://52.78.182.174:%d'%send_port, ['generator.py'])
 
 print '------------------------test1----------------------------'
+print time.ctime()
 
 # 等待结束
 job.wait_completion()
+
+print '------------------------test2----------------------------'
+print time.ctime()
 
 # 保存模型
 trained_model = job.get_trained_model()
